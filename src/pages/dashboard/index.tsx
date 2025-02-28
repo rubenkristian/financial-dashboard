0c30ea596cfe2f81649068308b99e36a6d344b69
rubenkristian96 <rubenkristian96@gmail.com>
2025-03-01 02:49:51 +0700

    feat: move file and generate fake data

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,100 +1,10 @@
 import MyCard from "../../components/container/MyCard";
 import RecentTransaction from "../../components/container/RecentTransaction";
-import WeeklyActivity, { WeeklyActivityData } from "../../components/container/WeeklyActivaty";
-import ExpenseStatistics, { ExpenseStatisticsData } from "../../components/container/ExpenseStatistics";
-import BalanceHistory, { BalanceHistoryData } from "../../components/container/BalanceHistory";
+import WeeklyActivity from "../../components/container/WeeklyActivaty";
+import ExpenseStatistics from "../../components/container/ExpenseStatistics";
+import BalanceHistory from "../../components/container/BalanceHistory";
 import QuickTransfer from "../../components/container/QuickTransfer";
-import { generateCardData, generateFakeChips } from "../../utils/data";
-
-const data: Array<WeeklyActivityData> = [
-  {
-    name: 'Sat',
-    deposit: 4000,
-    withdraw: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Sun',
-    deposit: 4000,
-    withdraw: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Mon',
-    deposit: 4000,
-    withdraw: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Tue',
-    deposit: 4000,
-    withdraw: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Wed',
-    deposit: 4000,
-    withdraw: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Thu',
-    deposit: 4000,
-    withdraw: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Fri',
-    deposit: 4000,
-    withdraw: 2400,
-    amt: 2400,
-  },
-];
-
-const dataPie: Array<ExpenseStatisticsData> = [
-  { label: 'Entertainment', value: 30 },
-  { label: 'Investment', value: 20 },
-  { label: 'Bill Expense', value: 15 },
-  { label: 'Others', value: 35 },
-];
-
-const dataLines: Array<BalanceHistoryData> = [
-  {
-    label: 'Jul',
-    value: 2400,
-    amt: 2400,
-  },
-  {
-    label: 'Aug',
-    value: 1398,
-    amt: 2210,
-  },
-  {
-    label: 'Sep',
-    value: 9800,
-    amt: 2290,
-  },
-  {
-    label: 'Oct',
-    value: 3908,
-    amt: 2000,
-  },
-  {
-    label: 'Nov',
-    value: 4800,
-    amt: 2181,
-  },
-  {
-    label: 'Dec',
-    value: 3800,
-    amt: 2500,
-  },
-  {
-    label: 'Jan',
-    value: 4300,
-    amt: 2100,
-  },
-];
+import { generateBalanceHistoryData, generateCardData, generateExpenseStatisticsData, generateFakeChips, generateWeeklyActivityData } from "../../utils/data";
 
 const DashboardIndexPage = () => {
     return (
@@ -109,13 +19,13 @@ const DashboardIndexPage = () => {
           </div>
             
           <div className='flex flex-wrap content:flex-nowrap gap-7-5'>
-            <WeeklyActivity data={data}/>
-            <ExpenseStatistics data={dataPie}/>
+            <WeeklyActivity data={generateWeeklyActivityData()}/>
+            <ExpenseStatistics data={generateExpenseStatisticsData()}/>
           </div>
             
           <div className='flex flex-wrap content:flex-nowrap gap-7-5'>
             <QuickTransfer/>
-            <BalanceHistory data={dataLines}/>
+            <BalanceHistory data={generateBalanceHistoryData()}/>
           </div>
         </div>
     );
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,6 +2,9 @@ import { faker } from "@faker-js/faker";
 import { TransactionData } from "../components/container/RecentTransaction";
 import { MyCardData } from "../components/container/MyCard";
 import { TransferData } from "../components/container/ContactList";
+import { WeeklyActivityData } from "../components/container/WeeklyActivaty";
+import { ExpenseStatisticsData } from "../components/container/ExpenseStatistics";
+import { BalanceHistoryData } from "../components/container/BalanceHistory";
 
 export function generateCardData(count: number): TransactionData[] {
     const types: TransactionData["type"][] = ["card", "paypal", "transfer"];
@@ -29,5 +32,33 @@ export function generateFakeTransfers(count: number): TransferData[] {
         name: faker.person.fullName(),
         position: faker.person.jobTitle(),
         img: faker.image.avatar(),
+    }));
+};
+
+export const generateWeeklyActivityData = (): WeeklyActivityData[] => {
+    const days = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+    return days.map((day) => ({
+        name: day,
+        deposit: faker.number.int({ min: 1000, max: 5000 }),
+        withdraw: faker.number.int({ min: 500, max: 3000 }),
+        amt: faker.number.int({ min: 500, max: 4000 }),
+    }));
+};
+
+export const generateExpenseStatisticsData = (): ExpenseStatisticsData[] => {
+    return [
+        { label: 'Entertainment', value: faker.number.int({ min: 10, max: 40 }) },
+        { label: 'Investment', value: faker.number.int({ min: 10, max: 30 }) },
+        { label: 'Bill Expense', value: faker.number.int({ min: 5, max: 20 }) },
+        { label: 'Others', value: faker.number.int({ min: 20, max: 50 }) },
+    ];
+};
+
+export const generateBalanceHistoryData = (): BalanceHistoryData[] => {
+    const months = ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+    return months.map((month) => ({
+        label: month,
+        value: faker.number.int({ min: 1000, max: 10000 }),
+        amt: faker.number.int({ min: 1500, max: 3000 }),
     }));
 };
